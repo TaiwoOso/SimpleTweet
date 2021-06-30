a49282cb926ae155e4bcfa75558078577828c779
taiwooso <oso_taiwo@yahoo.com>
2021-06-30 18:00:22 -0400

    Publish Tweet and Insert into List

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -13,12 +14,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+@Parcel
 public class Tweet {
     private static final String TAG = "Tweet";
 
     public String body;
     public String createdAt;
     public User user;
+
+    // empty constructor needed for Parcel
+    public Tweet() {}
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
